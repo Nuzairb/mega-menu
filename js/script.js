c6e72590f1c1d82620021da38bfd359bc32b381c
Nuzair Ibrahim <nuzairb@gmail.com>
2018-12-09 18:33:04 +0500

    finished js

--- js/script.js
+++ js/script.js
@@ -4,3 +4,7 @@ var closeMenu = document.getElementById("close-menu");
 document.getElementById("open-menu").addEventListener('click', function() {
     overlay.classList.add("show-menu");
 });
+
+document.getElementById("close-menu").addEventListener('click', function () {
+    overlay.classList.remove("show-menu");
+});
